9f95f824f0ea23aad93a691bb4f5c4d4999c8fe4
Xwaterboi <adir.arieli@gmail.com>
2025-01-13 09:40:18 +0200

    game end

--- game.py
+++ game.py
@@ -29,21 +29,21 @@ class game:
     def loop(self):
         """Resets the environment, agent, and game state to start a new game."""
         self.score = 0
-        
         # Reinitialize the agent and environment objects (reset their state)
         clock = pygame.time.Clock()
         background=Background(WINDOWWIDTH, WINDOWHEIGHT)
         env = Environment()
         background.render(env)
         player = Human_Agent()
-        
-        
+
+        self.duration = 30000
+        start_time = pygame.time.get_ticks()
         # Start the game loop for the new game
         run = True
-
+        win=False
         while run:
-            dt = clock.tick(FPS) / 1000.0  
-            #env.spawn_timer += dt  # add  time
+            dt = clock.tick(FPS) 
+            win=False
             pygame.event.pump()
 
             events = pygame.event.get()
@@ -52,6 +52,16 @@ class game:
                     run=False
                     pygame.quit()
                     exit()
+            elapsed_time = pygame.time.get_ticks() - start_time
+            if elapsed_time >= self.duration :
+                print("30 seconds! win!")
+                win=True
+                
+                
+            if env.score >=5:
+                print("5 points! win!")
+                win=True
+                
 
             action = player.getAction(events)  
 
@@ -69,6 +79,7 @@ class game:
 
             env.move(action=action)
             done = env.update()
+            done=win
             if done:
                 background.end_screen()
             else:
